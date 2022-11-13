31e4b1722bf50ebbf87f169a0c303f8298266fb3
baptistecdr <baptistecdr@users.noreply.github.com>
2022-11-13 12:53:13 +0100

    Release v4.0.1

---
+++ scripts/update-version.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require("fs");
+
+if (process.argv.length <= 2) {
+  console.error("You need to specify a new version.");
+  process.exit();
+}
+const version = process.argv[2];
+const filesToUpdate = ["package.json", "public/manifest.json", "package-lock.json"];
+
+filesToUpdate.forEach((file) => {
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) {
+      console.error(`Unable to read the content of the file '${file}'. (${err})`);
+    } else {
+      const content = JSON.parse(data);
+      content.version = version;
+      fs.writeFile(file, `${JSON.stringify(content, null, 2)}\n`, (err1) => {
+        if (err1) {
+          console.error(`Unable to write the new content of the file '${file}'. (${err1})`);
+        }
+      });
+    }
+  });
+});
