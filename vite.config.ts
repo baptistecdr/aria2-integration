25f55bb310690c5a90d9fd4bc706618160767f54
baptistecdr <baptistecdr@users.noreply.github.com>
2026-04-15 19:43:53 +0200

    Make build crossplatform and fix manifest generation (#1122)

--- vite.config.ts
+++ vite.config.ts
@@ -2,60 +2,62 @@
 import { execSync } from "node:child_process";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 const r = (...args: string[]) => resolve(__dirname, ...args);
 
-const minify = process.env.NODE_ENV === "production" ? "esbuild" : false;
-const cssMinify = process.env.NODE_ENV === "production" ? "esbuild" : false;
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  root: r("src"),
-  publicDir: r("public"),
-  build: {
-    target: "ES2023",
-    cssMinify,
-    minify,
-    rollupOptions: {
-      input: {
-        background: r("src", "background", "background.ts"),
-        options: r("src", "options", "options.html"),
-        popup: r("src", "popup", "popup.html"),
-      },
-      output: {
-        dir: r("dist"),
-        entryFileNames: "js/[name].js",
-        chunkFileNames: "js/[name].js",
-        assetFileNames: "media/[name].[ext]",
-      },
-    },
-  },
-  plugins: [
-    react(),
-    nodePolyfills(),
-    tsconfigPaths(),
-    {
-      name: "generate-manifest",
-      closeBundle() {
-        if (process.env.VITEST) return;
-        execSync("node scripts/generate-manifest.js", { stdio: "inherit", env: process.env });
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, r("."), "");
+  const minify = !mode.endsWith("-debug") && "esbuild";
+
+  return {
+    root: r("src"),
+    publicDir: r("public"),
+    build: {
+      target: "ES2023",
+      cssMinify: minify,
+      minify,
+      rollupOptions: {
+        input: {
+          background: r("src", "background", "background.ts"),
+          options: r("src", "options", "options.html"),
+          popup: r("src", "popup", "popup.html"),
+        },
+        output: {
+          dir: r("dist"),
+          entryFileNames: "js/[name].js",
+          chunkFileNames: "js/[name].js",
+          assetFileNames: "media/[name].[ext]",
+        },
       },
     },
-  ],
-  test: {
-    root: r("."),
-    environment: "jsdom",
-    globals: true,
-    setupFiles: [r("test", "setupTests.ts")],
-    coverage: {
-      reporter: ["text", "json", "json-summary", "html"],
-      reportsDirectory: r("coverage"),
+    plugins: [
+      react(),
+      nodePolyfills(),
+      tsconfigPaths(),
+      {
+        name: "generate-manifest",
+        closeBundle() {
+          if (process.env.VITEST) return;
+          execSync("node scripts/generate-manifest.js", { stdio: "inherit", env: { ...process.env, ...env } });
+        },
+      },
+    ],
+    test: {
+      root: r("."),
+      environment: "jsdom",
+      globals: true,
+      setupFiles: [r("test", "setupTests.ts")],
+      coverage: {
+        reporter: ["text", "json", "json-summary", "html"],
+        reportsDirectory: r("coverage"),
+      },
+      chaiConfig: {
+        truncateThreshold: 0,
+      },
     },
-    chaiConfig: {
-      truncateThreshold: 0,
-    },
-  },
+  };
 });
