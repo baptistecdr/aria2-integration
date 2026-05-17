8ad81424403b178b39dece9bb97f85fff99c92d9
baptistecdr <baptistecdr@users.noreply.github.com>
2026-05-17 08:20:36 +0200

    Remove vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { execSync } from "node:child_process";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { defineConfig, loadEnv } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 const r = (...args: string[]) => resolve(__dirname, ...args);
 
@@ -15,6 +14,11 @@ export default defineConfig(({ mode }) => {
   return {
     root: r("src"),
     publicDir: r("public"),
+    resolve: {
+      alias: {
+        "@": r("src"),
+      },
+    },
     build: {
       target: "ES2023",
       cssMinify: minify,
@@ -36,7 +40,6 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [
       react(),
-      tsconfigPaths(),
       {
         name: "generate-manifest",
         closeBundle() {
